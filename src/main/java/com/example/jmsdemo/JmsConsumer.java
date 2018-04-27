35ec6abdc4f8b5f99c896853f03340f1d3d6a6ae
estarter <estarter@gmail.com>
2018-04-27 23:48:20 +0200

    clean up

--- src/main/java/com/example/jmsdemo/JmsConsumer.java
+++ src/main/java/com/example/jmsdemo/JmsConsumer.java
@@ -4,13 +4,14 @@ import java.util.Random;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.listener.SessionAwareMessageListener;
 
-public class JmsConsumer implements MessageListener {
+public class JmsConsumer implements SessionAwareMessageListener {
     private static final Logger logger = LoggerFactory.getLogger(JmsConsumer.class);
     private String id;
 
@@ -21,25 +22,26 @@ public class JmsConsumer implements MessageListener {
     }
 
     @Override
-    public void onMessage(Message message) {
-        logger.info("{}: consuming message {}", id, message);
+    public void onMessage(Message omessage, Session session) throws JMSException {
+        TextMessage message = (TextMessage) omessage;
+        logger.info("{}: consuming message {}", id, message.getText());
         try {
             Thread.sleep(new Random().nextInt(4000));
         } catch (InterruptedException e) {}
         if (new Random().nextInt(5) > 1) {
-            logger.info("{}: finish message {}", id, message);
-//            try {
-//                session.commit();
-//            } catch (JMSException e) {
-//                logger.error("can't commit the message", e);
-//            }
+            logger.info("{}: finish message {}", id, message.getText());
+            try {
+                session.commit();
+            } catch (JMSException e) {
+                logger.error("can't commit the message", e);
+            }
         } else {
-            logger.info("{}: reschedule message {}", id, message);
-//            try {
-//                session.rollback();
-//            } catch (JMSException e) {
-//                logger.error("can't rollback the message", e);
-//            }
+            logger.info("{}: reschedule message {}", id, message.getText());
+            try {
+                session.rollback();
+            } catch (JMSException e) {
+                logger.error("can't rollback the message", e);
+            }
         }
     }
 
--- src/main/java/com/example/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/example/jmsdemo/JmsDemoApplication.java
@@ -9,60 +9,11 @@ import org.springframework.jms.listener.DefaultMessageListenerContainer;
 @SpringBootApplication
 @EnableJms
 @ImportResource("classpath:camel.xml")
-public class JmsDemoApplication /*implements JmsListenerConfigurer*/ {
+public class JmsDemoApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(JmsDemoApplication.class);
-
-//        BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(JmsConsumer.class);
-//        builder.addPropertyReference("propertyName", "consumer2");  // add dependency to other bean
-//        builder.addPropertyValue("propertyName", someValue);      // set property value
-//        DefaultListableBeanFactory factory = (DefaultListableBeanFactory) context.getBeanFactory();
-//        factory.registerBeanDefinition("beanName", builder.getBeanDefinition());
     }
 
-//    @Bean
-//    ServletRegistrationBean servletRegistrationBean() {
-//        ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(),
-//                "camel/*");
-//        servlet.setName("CamelServlet");
-//        return servlet;
-//    }
-
-
-//    @Autowired
-//    ConnectionFactory connectionFactory;
-//
-//    @Bean
-//    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        return factory;
-//    }
-//
-//    @Bean
-//    public DefaultMessageListenerContainer listener() {
-//        DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.setDestinationName("input-queue");
-//        container.setMessageListener(new JmsConsumer("1"));
-//        container.setSessionTransacted(true);
-//        container.setConcurrentConsumers(5);
-//        container.setMaxConcurrentConsumers(5);
-//        return container;
-//    }
-
-//    @Override
-//    public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
-//        Function<String, JmsListenerEndpoint> f = (id) -> {
-//            SimpleJmsListenerEndpoint endpoint = new SimpleJmsListenerEndpoint();
-//            endpoint.setId("myJmsEndpoint_"+id);
-//            endpoint.setDestination("input-queue");
-//            endpoint.setMessageListener(new JmsConsumer(id));
-//            return endpoint;
-//        };
-//        registrar.registerEndpoint(f.apply("1"));
-//        registrar.registerEndpoint(f.apply("2"));
-//    }
 }
