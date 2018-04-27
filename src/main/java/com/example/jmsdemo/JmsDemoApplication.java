811931c80bbf874684212592467dafad3177c00e
estarter <estarter@gmail.com>
2018-04-27 23:32:18 +0200

    make camel run

--- src/main/java/com/example/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/example/jmsdemo/JmsDemoApplication.java
@@ -1,38 +1,18 @@
 package com.example.jmsdemo;
 
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
-
-import javax.annotation.PostConstruct;
-import javax.jms.ConnectionFactory;
-
-import org.springframework.aop.target.AbstractBeanFactoryBasedTargetSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.annotation.JmsListenerConfigurer;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerEndpoint;
-import org.springframework.jms.config.JmsListenerEndpointRegistrar;
-import org.springframework.jms.config.MethodJmsListenerEndpoint;
-import org.springframework.jms.config.SimpleJmsListenerEndpoint;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
-import org.springframework.messaging.Message;
 
 @SpringBootApplication
 @EnableJms
+@ImportResource("classpath:camel.xml")
 public class JmsDemoApplication /*implements JmsListenerConfigurer*/ {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(JmsDemoApplication.class, args);
+        SpringApplication.run(JmsDemoApplication.class);
 
 //        BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(JmsConsumer.class);
 //        builder.addPropertyReference("propertyName", "consumer2");  // add dependency to other bean
@@ -41,28 +21,36 @@ public class JmsDemoApplication /*implements JmsListenerConfigurer*/ {
 //        factory.registerBeanDefinition("beanName", builder.getBeanDefinition());
     }
 
+//    @Bean
+//    ServletRegistrationBean servletRegistrationBean() {
+//        ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(),
+//                "camel/*");
+//        servlet.setName("CamelServlet");
+//        return servlet;
+//    }
 
-    @Autowired
-    ConnectionFactory connectionFactory;
 
-    @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        return factory;
-    }
-
-    @Bean
-    public DefaultMessageListenerContainer listener() {
-        DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setDestinationName("input-queue");
-        container.setMessageListener(new JmsConsumer("1"));
-        container.setSessionTransacted(true);
-        container.setConcurrentConsumers(5);
-        container.setMaxConcurrentConsumers(5);
-        return container;
-    }
+//    @Autowired
+//    ConnectionFactory connectionFactory;
+//
+//    @Bean
+//    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+//        factory.setConnectionFactory(connectionFactory);
+//        return factory;
+//    }
+//
+//    @Bean
+//    public DefaultMessageListenerContainer listener() {
+//        DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.setDestinationName("input-queue");
+//        container.setMessageListener(new JmsConsumer("1"));
+//        container.setSessionTransacted(true);
+//        container.setConcurrentConsumers(5);
+//        container.setMaxConcurrentConsumers(5);
+//        return container;
+//    }
 
 //    @Override
 //    public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
