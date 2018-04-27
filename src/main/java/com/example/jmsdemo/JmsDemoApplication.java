68c44a9b8c4849927da233d2fbad5f58bf8d772c
estarter <estarter@gmail.com>
2018-04-27 23:42:13 +0200

    link camel and consumer

--- src/main/java/com/example/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/example/jmsdemo/JmsDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @SpringBootApplication
 @EnableJms
