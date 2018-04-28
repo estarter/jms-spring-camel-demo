ce5fc9d6c1c56e1fa9fda63967bfc9b845c9db49
estarter <estarter@gmail.com>
2018-04-28 09:24:58 +0200

    configure prefetch policy in code

--- src/main/java/com/example/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/example/jmsdemo/JmsDemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.jmsdemo;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQPrefetchPolicy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
 
@@ -12,6 +18,18 @@ public class JmsDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JmsDemoApplication.class);
+    }
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Bean
+    public ConnectionFactory jmsConnectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
+        ActiveMQPrefetchPolicy policy = new ActiveMQPrefetchPolicy();
+        policy.setQueuePrefetch(1);
+        factory.setPrefetchPolicy(policy);
+        return factory;
     }
 
 }
